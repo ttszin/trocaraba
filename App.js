e29752daf9db5e190bd196a7eab65e155bc50dcc
Matheus Teixeira de Souza <95655266+ttszin@users.noreply.github.com>
2022-10-20 08:56:25 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './Componentes/Header.js';
+import Body from './Componentes/Body.js';
+import Footer from './Componentes/Footer.js';
+import StyleExterno from './styles.js';
+
+export default class App extends React.Component{
+  render(){
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
+
+
---
+++ styles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+
+    botaoexemplo:{
+        backgroundColor:'blue',
+        borderBottomColor:'red',
+        borderBottomWidth:10
+    },
+
+
+
+});
